905cb87d1e91265f8791eb7785eddcb91c3fcb98
Ahmed Sabahi <a7md2shrf@gmail.com>
2021-04-03 00:33:54 +0200

    grid images with text overlay on it

--- lib/UI/popularScreen.dart
+++ lib/UI/popularScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:list_of_popular/UI/popularProvider.dart';
 import 'package:provider/provider.dart';
 
@@ -18,12 +19,43 @@ class PopularScreen extends StatelessWidget {
         child: Consumer<PopularProvider>(
           builder: (buildContext, popularProvider, _) {
             return (popularProvider.list != null)
-                ? ListView.builder(
+                ? GridView.builder(
+                    shrinkWrap: true,
+                    padding: EdgeInsets.all(5.0),
                     itemCount: popularProvider.list.results.length,
-                    itemBuilder: (ctx, index) {
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 5.0,
+                      crossAxisSpacing: 5.0,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
                       final popular = popularProvider.list.results[index];
-                      return Card(
-                        child: Text(popular.name),
+                      return Hero(
+                        tag: popular.id,
+                        child: Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                    'https://image.tmdb.org/t/p/w500${popular.profilePath}',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              popular.name,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  backgroundColor: Colors.black54,
+                                  color: Colors.white,
+                                  fontSize: 17.0),
+                            ),
+                          ],
+                        ),
                       );
                     },
                   )
@@ -34,3 +66,45 @@ class PopularScreen extends StatelessWidget {
     );
   }
 }
+//
+// Flexible(
+// child: Column(
+// crossAxisAlignment:
+// CrossAxisAlignment.start,
+// children: [
+// Text(
+// popular.name,
+// style: TextStyle(
+// fontWeight: FontWeight.bold,
+// fontSize: 18,
+// ),
+// ),
+// ],
+// ),
+// ),
+//
+// GridView.count(
+// crossAxisCount: 2,
+// mainAxisSpacing: 10.0,
+// crossAxisSpacing: 10.0,
+// padding: EdgeInsets.all(5.0),
+// children: List.generate(popularProvider.list.results.length,
+// (index) {
+// final popular = popularProvider.list.results[index];
+// return Center(
+// child: Hero(
+// tag: popular.id,
+// child: Container(
+// decoration: BoxDecoration(
+// image: DecorationImage(
+// fit: BoxFit.cover,
+// image: NetworkImage(
+// 'https://image.tmdb.org/t/p/w500${popular.profilePath}',
+// ),
+// ),
+// ),
+// ),
+// ),
+// );
+// }),
+// )
--- lib/services/popularService.dart
+++ lib/services/popularService.dart
@@ -6,7 +6,6 @@ class PopularService {
     var url = Uri.parse(
         'https://api.themoviedb.org/3/person/popular?api_key=6557d01ac95a807a036e5e9e325bb3f0&language=en-US&page=1');
     final response = await http.get(url);
-    print(response);
     if (response.statusCode == 200) {
       print(response.body);
       return popularFromJson(response.body);
