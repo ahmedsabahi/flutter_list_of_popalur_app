865831cc04d544205d06143433d4719a3645e504
Ahmed Sabahi <a7md2shrf@gmail.com>
2021-04-03 18:08:27 +0200

    download image feature

---
+++ lib/UI/ImageScreen.dart
@@ -0,0 +1,75 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:dio/dio.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ImageScreen extends StatelessWidget {
+  final int id;
+  final String image;
+  final String name;
+
+  const ImageScreen({
+    Key key,
+    @required this.id,
+    @required this.image,
+    @required this.name,
+  }) : super(key: key);
+
+  _save() async {
+    if (await Permission.storage.request().isGranted) {
+      var response = await Dio()
+          .get(image, options: Options(responseType: ResponseType.bytes));
+      final result = await ImageGallerySaver.saveImage(
+          Uint8List.fromList(response.data),
+          quality: 60,
+          name: name);
+      print(result);
+      _toastInfo("You have successfully added to your gallery.");
+    }
+  }
+
+  _toastInfo(String info) {
+    Fluttertoast.showToast(
+        msg: info,
+        toastLength: Toast.LENGTH_LONG,
+        backgroundColor: Colors.green);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                _save();
+              },
+              child: Icon(
+                Icons.save,
+                size: 26.0,
+              ),
+            ),
+          ),
+        ],
+        title: Text(name),
+      ),
+      body: Hero(
+        tag: id,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                image,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/UI/popularScreen.dart
+++ lib/UI/popularScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:list_of_popular/UI/ImageScreen.dart';
 import 'package:list_of_popular/UI/popularProvider.dart';
 import 'package:provider/provider.dart';
 
@@ -30,31 +31,43 @@ class PopularScreen extends StatelessWidget {
                     ),
                     itemBuilder: (BuildContext context, int index) {
                       final popular = popularProvider.list.results[index];
-                      return Hero(
-                        tag: popular.id,
-                        child: Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(
-                                    'https://image.tmdb.org/t/p/w500${popular.profilePath}',
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => ImageScreen(
+                                  id: popular.id,
+                                  image:
+                                      'https://image.tmdb.org/t/p/w500${popular.profilePath}',
+                                  name: popular.name)));
+                        },
+                        child: Hero(
+                          tag: popular.id,
+                          child: Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                      'https://image.tmdb.org/t/p/w500${popular.profilePath}',
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Text(
-                              popular.name,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  backgroundColor: Colors.black54,
-                                  color: Colors.white,
-                                  fontSize: 17.0),
-                            ),
-                          ],
+                              ElevatedButton(
+                                child: Text(
+                                  popular.name,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      backgroundColor: Colors.black54,
+                                      color: Colors.white,
+                                      fontSize: 16.0),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
@@ -66,45 +79,3 @@ class PopularScreen extends StatelessWidget {
     );
   }
 }
-//
-// Flexible(
-// child: Column(
-// crossAxisAlignment:
-// CrossAxisAlignment.start,
-// children: [
-// Text(
-// popular.name,
-// style: TextStyle(
-// fontWeight: FontWeight.bold,
-// fontSize: 18,
-// ),
-// ),
-// ],
-// ),
-// ),
-//
-// GridView.count(
-// crossAxisCount: 2,
-// mainAxisSpacing: 10.0,
-// crossAxisSpacing: 10.0,
-// padding: EdgeInsets.all(5.0),
-// children: List.generate(popularProvider.list.results.length,
-// (index) {
-// final popular = popularProvider.list.results[index];
-// return Center(
-// child: Hero(
-// tag: popular.id,
-// child: Container(
-// decoration: BoxDecoration(
-// image: DecorationImage(
-// fit: BoxFit.cover,
-// image: NetworkImage(
-// 'https://image.tmdb.org/t/p/w500${popular.profilePath}',
-// ),
-// ),
-// ),
-// ),
-// ),
-// );
-// }),
-// )
